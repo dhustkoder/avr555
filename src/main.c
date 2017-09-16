148957ffa68a0d3d49bcb32388429cc4c02b5511
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-09-15 22:02:18 -0300

    avr555 working
    rev

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
-#include <stdnoreturn.h>
 #include <stdbool.h>
+#include <stdnoreturn.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include "uart.h"
 
 /* *
  * 555 timer emulator for AVR by Rafael Moura (https://github.com/dhustkoder)
@@ -10,69 +9,37 @@
  * */
 
 enum Pins {
-	ANALOG_PIN_TRIGGER = 0x00,           /* input */
-	ANALOG_PIN_THRESHOLD = 0x01<<MUX0,   /* input */
-	DIGITAL_PIN_OUTPUT = 0x01<<PD3,      /* output */
-	DIGITAL_PIN_RESET = 0x01<<PD2,       /* input  */
-	DIGITAL_PIN_DISCHARGE = 0x01<<PD4    /* output */
+	ANALOG_PIN_TR   = 0x00,
+	ANALOG_PIN_TH   = 0x01<<MUX0,
+	DIGITAL_PIN_DC  = 0x01<<PD2,
+	DIGITAL_PIN_OUT = 0x01<<PD3
 };
-
-
-static void avr555_init(void)
-{
-	/* digital setup */
-	DDRD |= DIGITAL_PIN_OUTPUT|DIGITAL_PIN_DISCHARGE;
-	DDRD &= ~DIGITAL_PIN_RESET;
-	PORTD |= DIGITAL_PIN_DISCHARGE;
-}
 
 static uint16_t read_adc(const uint8_t analog_pin)
 {
 	ADMUX = (0x01<<REFS0)|analog_pin;
-
 	ADCSRA = (0x01<<ADEN)|(0x01<<ADSC)|
-	  (0x01<<ADPS2)|(0x01<<ADPS1)|(0x01<<ADPS0);
+	         (0x01<<ADPS2)|(0x01<<ADPS1)|(0x01<<ADPS0);
 
 	while (ADCSRA&(0x01<<ADSC))
-		_delay_us(1);
+		;
 
 	return ADC;
 }
 
-static bool trigger_comparator_output(void)
-{
-	const uint16_t vplus = 341;
-	const uint16_t vminus = read_adc(ANALOG_PIN_TRIGGER);
-	return vplus > vminus;
-}
-
-static bool threshold_comparator_output(void)
-{
-	const uint16_t vminus = 682;
-	const uint16_t vplus = read_adc(ANALOG_PIN_THRESHOLD);
-	return vplus > vminus;
-}
-
-
 noreturn void main(void)
 {
-	avr555_init();
-	uart_init();
+	DDRD |= DIGITAL_PIN_OUT|DIGITAL_PIN_DC;
+	PORTD &= ~(DIGITAL_PIN_OUT|DIGITAL_PIN_DC);
 
 	for (;;) {
-		const bool flipflop_out = !trigger_comparator_output() &&
-			                   threshold_comparator_output();
-		const bool vout = !flipflop_out;
+		if (read_adc(ANALOG_PIN_TR) < 338)
+			PORTD |= DIGITAL_PIN_OUT|DIGITAL_PIN_DC;
 
-		if (flipflop_out)
-			PORTD &= ~DIGITAL_PIN_DISCHARGE;
-		else
-			PORTD |= DIGITAL_PIN_DISCHARGE;
-
-		if (vout)
-			PORTD |= DIGITAL_PIN_OUTPUT;
-		else
-			PORTD &= ~DIGITAL_PIN_OUTPUT;
+		if (read_adc(ANALOG_PIN_TH) > 686) {
+			PORTD &= ~(DIGITAL_PIN_OUT|DIGITAL_PIN_DC);
+			while (read_adc(ANALOG_PIN_TR) > 338)
+				;
+		}
 	}
-
 }
