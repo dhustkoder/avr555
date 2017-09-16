c41fd5d348a5241f6bb33ab7ac2dcb18115bf72d
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-09-16 09:07:33 -0300

    added reset pin

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdnoreturn.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 /* *
  * 555 timer emulator for AVR by Rafael Moura (https://github.com/dhustkoder)
@@ -9,11 +10,17 @@
  * */
 
 enum Pins {
-	ANALOG_PIN_TR   = 0x00,
-	ANALOG_PIN_TH   = 0x01<<MUX0,
-	DIGITAL_PIN_DC  = 0x01<<PD2,
-	DIGITAL_PIN_OUT = 0x01<<PD3
+	ANALOG_PIN_TR          = 0x00,
+	ANALOG_PIN_TH          = 0x01<<MUX0,
+	DIGITAL_PIN_DC         = 0x01<<PD4,
+	DIGITAL_PIN_OUT        = 0x01<<PD3,
+	DIGITAL_PIN_RES        = 0x01<<PD2,
+	DIGITAL_PIN_RES_INTMSK = 0x01<<INT0
 };
+
+
+static volatile bool reset;
+
 
 static uint16_t read_adc(const uint8_t analog_pin)
 {
@@ -27,10 +34,26 @@ static uint16_t read_adc(const uint8_t analog_pin)
 	return ADC;
 }
 
+
 noreturn void main(void)
 {
 	DDRD |= DIGITAL_PIN_OUT|DIGITAL_PIN_DC;
+	DDRD &= ~DIGITAL_PIN_RES;
+	PORTD |= DIGITAL_PIN_RES; /* pullup resistor */
+	
+	EICRA = 0x00; /* interrupt on pulled low */
+	EIMSK = DIGITAL_PIN_RES_INTMSK;
+
+Lreset:	
+	cli();
+
 	PORTD &= ~(DIGITAL_PIN_OUT|DIGITAL_PIN_DC);
+	while (!(PIND&DIGITAL_PIN_RES))
+		;
+
+	reset = false;
+
+	sei();
 
 	for (;;) {
 		if (read_adc(ANALOG_PIN_TR) < 338)
@@ -38,8 +61,18 @@ noreturn void main(void)
 
 		if (read_adc(ANALOG_PIN_TH) > 686) {
 			PORTD &= ~(DIGITAL_PIN_OUT|DIGITAL_PIN_DC);
-			while (read_adc(ANALOG_PIN_TR) > 338)
+			while (read_adc(ANALOG_PIN_TR) > 338 && !reset)
 				;
 		}
+
+		if (reset)
+			goto Lreset;
 	}
 }
+
+
+ISR(__vector_INT0_Vect)
+{
+	reset = true;
+}
+
